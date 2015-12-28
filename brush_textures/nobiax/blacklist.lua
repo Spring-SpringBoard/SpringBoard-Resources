12935f98f38098e772e5a23e9af8d1c7a8a88356
Gajo Petrovic <gajo01@gmail.com>
2015-12-28 14:48:52 +0900

    nobiax ground textures

---
+++ brush_textures/nobiax/blacklist.lua
@@ -0,0 +1,27 @@
+return {
+	-- grass & leafs
+	"pattern_85",
+	"pattern_87",
+	"pattern_88",
+	"pattern_89",
+	"pattern_90",
+	-- ok-ish
+	-- "pattern_91", 
+	"pattern_92",
+	"pattern_93",
+	
+	"pattern_163", 
+	"pattern_164",
+	"pattern_165",
+	"pattern_166",
+	"pattern_167",
+	"pattern_168",
+	
+	-- wood?
+	"pattern_149",
+	
+	-- ground with stone (bad texture)
+	"pattern_104",
+	-- has grass
+	"pattern_106",
+}
---
+++ brush_textures/nobiax/misc/blacklist.lua
@@ -0,0 +1,7 @@
+return {
+	-- not tiling
+	"pattern_264",
+	"pattern_264_diffuse_blue-orange",
+	"pattern_264_diffuse_red-green",
+	"pattern_264_diffuse_red-white",
+}
